42b0a320389e622fad4c0d486e0c818ea57b540b
Kavitha316S <316kavithadhana@gmail.com>
2023-05-13 20:33:02 +0530

    print

--- servers.tf
+++ servers.tf
@@ -1,58 +1,3 @@
-data "aws_ami" "centos" {
-  owners      = ["973714476881"]
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-}
-
-data "aws_security_group" "allow-all" {
-  name = "allow-all"
-}
-
-variable "components" {
-  default = {
-    frontend = {
-      name          = "frontend"
-      instance_type = "t3.small"
-    }
-    mongodb = {
-      name          = "mongodb"
-      instance_type = "t3.small"
-    }
-    catalogue = {
-      name          = "catalogue"
-      instance_type = "t3.micro"
-    }
-    redis = {
-      name          = "redis"
-      instance_type = "t3.small"
-    }
-    user = {
-      name          = "user"
-      instance_type = "t3.micro"
-    }
-    cart = {
-      name          = "cart"
-      instance_type = "t3.micro"
-    }
-    mysql = {
-      name          = "mysql"
-      instance_type = "t3.small"
-    }
-    shipping = {
-      name          = "shipping"
-      instance_type = "t3.medium"
-    }
-    rabbitmq = {
-      name          = "rabbitmq"
-      instance_type = "t3.small"
-    }
-    payment = {
-      name          = "payment"
-      instance_type = "t3.small"
-    }
-  }
-}
-
 resource "aws_instance" "instance" {
   for_each               = var.components
   ami                    = data.aws_ami.centos.image_id
@@ -66,10 +11,10 @@ resource "aws_instance" "instance" {
 
 
 resource "aws_route53_record" "records" {
-  for_each               = var.components
-  zone_id = "Z09384792YHLH982HW2W9"
-  name    = "${each.value["name"]}-dev.devopsb62.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instance[each.value["name"]].private_ip]
+  for_each = var.components
+  zone_id  = "Z09384792YHLH982HW2W9"
+  name     = "${each.value["name"]}-dev.devopsb62.online"
+  type     = "A"
+  ttl      = 30
+  records  = [aws_instance.instance[each.value["name"]].private_ip]
 }
