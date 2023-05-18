c089a5b107751168d570db3cac02f083e4491d6a
Kavitha316S <316kavithadhana@gmail.com>
2023-05-19 00:49:15 +0530

    print

--- module/servers.tf
+++
@@ -1,9 +0,0 @@
-module "servers" {
-  for_each = var.components
-
-  source         = "./module"
-  component_name = each.value["name"]
-  env            = var.env
-  instance_type  = each.value["instance_type"]
-  password       = lookup(each.value, "password", "null")
-}
