1d40b6fb6526165044fc2cbd46ef7a83d3f46207
Kavitha316S <316kavithadhana@gmail.com>
2023-05-19 03:38:08 +0530

    print

--- module/main.tf
+++ module/main.tf
@@ -32,7 +32,7 @@ resource "null_resource" "provisioner" {
 
 resource "aws_route53_record" "records" {
   zone_id = "Z09384792YHLH982HW2W9"
-  name    = "${var.component_name}-dev.rdevopsb72.online"
+  name    = "${var.component_name}-dev.devopsb62.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]
