9611c5c32485decf8c220e8965ba2c915065ec97
Kavitha316S <316kavithadhana@gmail.com>
2023-06-11 04:51:28 +0530

    print

--- module/locals.tf
+++ module/locals.tf
@@ -10,4 +10,11 @@ locals {
     "sudo labauto ansible",
     "ansible-pull -i localhost, -U https://github.com/Lavikavi/roboshop-ansible.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
+  db_tags = {
+    Name = "${var.component_name}-${var.env}"
+  }
+  app_tags = {
+    Name = "${var.component_name}-${var.env}"
+    Monitor = "true"
+  }
 }
