22d648fa7669efffe9a596e2b67da0f37b81706a
Kavitha316S <316kavithadhana@gmail.com>
2023-06-15 16:42:19 +0530

    print

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://github.com/Lavikavi/tf-module-vpc.git"
+  source = "git::https://github.com/raghudevopsb72/tf-module-vpc-1.git"
 
   for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
@@ -13,5 +13,23 @@ module "vpc" {
 }
 
 
+module "app" {
+  source = "git::https://github.com/raghudevopsb72/tf-module-app1.git"
+
+  for_each         = var.app
+  instance_type    = each.value["instance_type"]
+  name             = each.value["name"]
+  desired_capacity = each.value["desired_capacity"]
+  max_size         = each.value["max_size"]
+  min_size         = each.value["min_size"]
+
+  env          = var.env
+  bastion_cidr = var.bastion_cidr
+  tags         = local.tags
+
+  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
+}
 
 
